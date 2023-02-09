8e16ca5852ca48e5bb57b71f0813880071556be1
EmployedRanger <119915465+EmployedRanger@users.noreply.github.com>
2023-02-09 16:50:42 -0700

    Create choicePlayer and choiceComputer functions

---
+++ javascript.js
@@ -0,0 +1,21 @@
+// Rock Paper Scissors
+
+// Get computer choice
+function getComputerChoice() {
+//  randomly choose value 1, 2, 3
+    choiceComputer = Math.floor(Math.random() * 3);
+//  converts to 1, 2, 3 
+
+
+//  returns choice
+    console.log(choiceComputer);
+}
+
+// Get Player choice
+function getPlayerChoice() {
+//  prompt to get choice
+//  change choice to lower case
+//  return choice
+
+}
+
