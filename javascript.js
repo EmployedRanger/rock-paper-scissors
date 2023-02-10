0abfdadde9cf9dfcdeeff05301ace25680669823
EmployedRanger <119915465+EmployedRanger@users.noreply.github.com>
2023-02-09 23:20:46 -0700

    Updated game function

--- javascript.js
+++ javascript.js
@@ -1,21 +1,21 @@
 // Rock Paper Scissors
-
-// Get computer choice
-function getComputerChoice() {
-//  randomly choose value 1, 2, 3
-    choiceComputer = Math.floor(Math.random() * 3);
-//  converts to 1, 2, 3 
-
-
-//  returns choice
-    console.log(choiceComputer);
+function rockPaperScissors() {
+    // Get computer choice
+    function getComputerChoice() {
+    //  randomly choose value 1, 2, 3
+        choiceComputer = Math.floor(Math.random() * 3);
+    //  converts to 1, 2, 3 
+    
+    
+    //  returns choice
+        console.log(choiceComputer);
+    }
+    
+    // Get Player choice
+    function getPlayerChoice() {
+    //  prompt to get choice
+    //  change choice to lower case
+    //  return choice
+    
+    }
 }
-
-// Get Player choice
-function getPlayerChoice() {
-//  prompt to get choice
-//  change choice to lower case
-//  return choice
-
-}
-
